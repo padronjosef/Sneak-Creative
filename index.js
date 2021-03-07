9006667a0e4e986b97e8df20397116d58c765267
Jose Padron <josepadron.go@gmail.com>
2021-03-06 19:53:57 -0500

    improving logic and css

--- index.js
+++ index.js
@@ -1,54 +1,85 @@
 const navBar = document.getElementById("navbar__list");
-const showMore = document.getElementById("show-more");
-const navbarItems = navBar.getElementsByClassName("navbar__item");
+const navBarLink = navBar.getElementsByClassName("navbar__link");
+const navBarItems = navBar.getElementsByClassName("navbar__item");
 
+const hamburger = document.getElementById("hamburger")
+const header = document.getElementById("header")
 const gallery = document.getElementById('gallery')
-const fragment = document.createDocumentFragment()
+const showMore = document.getElementById("show-more");
 
-const searchBtn = document.getElementById("search")
-const imput = document.getElementById("navbar__imput")
-
-const clientId = "&client_id=IfN-L8JK5qNpABCzhE_FKhkR1JOvLLnHqzfdWVoeC5c"
+const searchIcon = document.getElementById("search__icon")
+const searchImput = document.getElementById("search__imput")
 
 let page = 1;
 let topic = "all"
 
-// const url = `https://api.unsplash.com/photos?page=${page}${clientId}`
-
-document.getElementById("hamburger").addEventListener("click", () => {
+// buger button logic
+hamburger.addEventListener("click", () => {
   document.getElementById("header").classList.toggle("is-active");
   header.classList.toggle("header--active");
 });
 
-searchBtn.addEventListener("click", () => {
-  (imput.classList != "navbar__imput--active")
-  ? imput.classList.add("navbar__imput--active")
-  : topic = imput.textContent
-})
+const closeHeader = () => {
+  if (header.classList.contains("header--active")) header.classList.remove("header--active");
+}
 
-for (let i = 0; i < navbarItems.length; i++) {
-  navbarItems[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("navbar__item--active");
-    current[0].className = current[0].className.replace("navbar__item--active");
-    header.classList.toggle("header--active");
-    this.className += " navbar__item--active";
+// navbar logic
+for (let i = 0; i < navBarLink.length; i++) {
+  navBarLink[i].addEventListener('click', navIconsSearch);
+}
+
+const classReplacer = (e) => {
+  let itemActive = document.getElementsByClassName("navbar__link--active");
+
+  itemActive[0].className = itemActive[0].className.replace("navbar__link--active" , "navbar__link");
+  e.target.className = "navbar__link--active";
+}
+
+const deleteImgs = () =>{
+  let img = document.getElementsByClassName("gallery__link");
+  while(img.length > 0){
+    img[0].parentNode.removeChild(img[0]);
+  }
+}
+
+function navIconsSearch(res) {
+  topic = this.textContent
+  page = 0
+  deleteImgs()
+  loadImgs(res)
+}
+
+for (let i = 0; i < navBarItems.length; i++) {
+  navBarLink[i].addEventListener("click", (e) => {
+    classReplacer(e)
+    closeHeader()
   });
 }
 
-const gridView = document.getElementById("grid-icon")
-  .addEventListener("click", () => {
-    if (gallery.classList.contains("gallery--inline")) gallery.classList.remove("gallery--inline")
-  });
+// display buttons
+const galleryAnimation = () => {
+  gallery.classList.add("fade-in");
+  setTimeout( ()=> {
+    gallery.classList.remove("fade-in");
+  },2000)
+}
 
-const listView = document.getElementById("list-icon")
-  .addEventListener("click", () => {
-    if (gallery.classList != "gallery--inline") gallery.classList.add("gallery--inline")
-  });
+const gridView = document.getElementById("grid-icon").addEventListener("click", () => {
+  if (gallery.classList.contains("gallery--inline")) gallery.classList.remove("gallery--inline")
+  galleryAnimation()
+});
 
-const fetchImgs = (res,firstColumn) => {
-  firstColumn = document.getElementById('first-column')
+const listView = document.getElementById("list-icon") .addEventListener("click", () => {
+  if (gallery.classList.contains("gallery--inline") == false) gallery.classList.add("gallery--inline")
+  galleryAnimation()
+});
+
+// gallery logic
+const ImgOrganizer = (res) => {
+  const firstColumn = document.getElementById('first-column')
   const secondColumn = document.getElementById('second-column')
   const thirdColumn = document.getElementById('third-column')
+  const fragment = document.createDocumentFragment()
 
   for (const photoInfo of res.data.results) {
     const link = document.createElement('FIGURE')
@@ -65,32 +96,51 @@ const fetchImgs = (res,firstColumn) => {
     let dataIndex = res.data.results.indexOf(photoInfo)
 
     if (dataIndex % 3 == 0) firstColumn.appendChild(link)
-    else if (dataIndex % 2 == 0) thirdColumn.appendChild(link)
-    else secondColumn.appendChild(link)
+    else if (dataIndex % 2 == 0) secondColumn.appendChild(link)
+    else thirdColumn.appendChild(link)
+
+    gallery.appendChild(fragment)
   }
-  gallery.appendChild(fragment)
+
+  if (res.data.results.length == 0) {
+    const link = document.createElement('FIGURE')
+    link.className = 'gallery__link'
+
+    const photo = document.createElement('IMG')
+    photo.src = "./assets/icons/not-found.png"
+    photo.alt = "not found"
+    photo.className = 'gallery__img'
+
+    link.appendChild(photo)
+    gallery.appendChild(link)
+    gallery.classList.add("gallery--inline")
+  }
+  galleryAnimation()
   page++
 }
 
 const loadImgs = () => {
+  const clientId = "&client_id=IfN-L8JK5qNpABCzhE_FKhkR1JOvLLnHqzfdWVoeC5c"
   let url = `https://api.unsplash.com/search/photos?page=${page}&query=${topic}${clientId}`
   axios({
     url: url
-  }).then(fetchImgs)
+  }).then(ImgOrganizer)
   .catch(console.log)
-  console.log(url)
 }
 
-for (let i = 0; i < navbarItems.length; i++) {
-  navbarItems[i].addEventListener('click', printDetails);
-}
+// Search imput logic
+searchImput.addEventListener("keypress", (e) => {
+  if (e.key === 'Enter') {
+    topic = e.target.value;
+    page = 0
+    deleteImgs()
+    loadImgs()
+  }
+})
 
-function printDetails(res) {
-  topic = this.textContent
-  loadImgs(res)
-  return page = 0
-}
+searchIcon.addEventListener("click", () => searchImput.classList.toggle("search__imput--active"))
 
+// Load More Btn Logic
 showMore.addEventListener('click', loadImgs)
 
 loadImgs()
